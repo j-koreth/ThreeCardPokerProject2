d7ddb65973aaedf9043d396fbcafedd670c51b92
Joel Koreth <joelsymon@gmail.com>
2020-02-29 17:29:56 -0600

    Fixed Deck

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck extends ArrayList<Card> {
-    ArrayList<Card> cards;
-
     Deck(){
         GenerateDeck();
     }
@@ -14,13 +12,13 @@ public class Deck extends ArrayList<Card> {
 
     //Generate new deck of cards in a random order
     void GenerateDeck(){
-        cards = new ArrayList<Card>();
-
         for(char c : Card.availableSuits()){
             for(int x: Card.availableValues()){
-                cards.add(new Card(c, x));
+                this.add(new Card(c, x));
             }
         }
-        Collections.shuffle(cards);
+        Collections.shuffle(this);
     }
+
+
 }
