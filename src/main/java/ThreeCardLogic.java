dad12e1df7a2409549c7aa7ef51cdf96e57fe897
Joel Koreth <joelsymon@gmail.com>
2020-02-29 19:28:21 -0600

    Testing Eval

--- src/main/java/ThreeCardLogic.java
+++ src/main/java/ThreeCardLogic.java
@@ -1,7 +1,14 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ThreeCardLogic {
     public static int evalHand(ArrayList<Card> hand){
+        ArrayList ch = new ArrayList();
+        for(Card card : hand){
+            ch.add(hand.toString());
+        }
+        Collections.sort(ch);
+        System.out.println(hand);
         return 0;
     }
 
